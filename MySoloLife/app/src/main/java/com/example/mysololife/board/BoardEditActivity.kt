07c71a3f3a432125c7778b122a791ad6f1a8f198
baekcii <bsy8470@gmail.com>
2025-05-12 22:27:10 +0900

    feat(boardEdit): 게시글 수정 구현

---
+++ MySoloLife/app/src/main/java/com/example/mysololife/board/BoardEditActivity.kt
@@ -0,0 +1,129 @@
+package com.example.mysololife.board
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.example.mysololife.R
+import com.example.mysololife.databinding.ActivityBoardEditBinding
+import com.example.mysololife.utils.FBAuth
+import com.example.mysololife.utils.FBRef
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+
+class BoardEditActivity : AppCompatActivity() {
+
+    private val TAG = BoardEditActivity::class.java.simpleName
+    private lateinit var binding: ActivityBoardEditBinding
+    private lateinit var key: String
+    private var curImg: Drawable? = null
+    private lateinit var uid: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_edit)
+
+        key = intent.getStringExtra("key").toString()
+        getBoardData(key)
+        getImageData(key)
+
+
+        binding.writeBtn.setOnClickListener {
+            val title = binding.titleArea.text.toString()
+            val content = binding.contentArea.text.toString()
+            val time = FBAuth.getTime()
+
+            FBRef.boardRef.child(key).setValue(BoardModel(title, content, uid, time))
+
+            Toast.makeText(this, "게시글 수정 완료", Toast.LENGTH_LONG).show()
+
+            if (curImg != null && binding.imgArea.drawable.constantState != curImg!!.constantState) {
+                imgUpload(key)
+                Toast.makeText(this, "이미지 업로드", Toast.LENGTH_SHORT).show()
+            }
+            finish()
+        }
+        binding.imgArea.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+        }
+    }
+
+    private fun imgUpload(key: String) {
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val mountainsRef = storageRef.child(key + ".png")
+
+        val imageView = binding.imgArea
+
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
+    }
+
+    private fun getImageData(key: String) {
+        val storageReference = Firebase.storage.reference.child(key + ".png")
+
+        val imageView = binding.imgArea
+
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Glide.with(this).load(task.result).into(imageView)
+                curImg = binding.imgArea.drawable
+            } else {
+
+            }
+        })
+    }
+
+    private fun getBoardData(key: String) {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val dataModel = dataSnapshot.getValue(BoardModel::class.java)
+                if (dataModel != null) {
+                    binding.titleArea.setText(dataModel.title)
+                    binding.contentArea.setText(dataModel.content)
+                    uid = dataModel.uid
+                } else {
+                    finish()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.boardRef.child(key).addValueEventListener(postListener)
+    }
+}
--- MySoloLife/app/src/main/java/com/example/mysololife/board/BoardInsideActivity.kt
+++ MySoloLife/app/src/main/java/com/example/mysololife/board/BoardInsideActivity.kt
@@ -1,9 +1,14 @@
 package com.example.mysololife.board
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -24,24 +29,38 @@ class BoardInsideActivity : AppCompatActivity() {
     private val TAG = BoardInsideActivity::class.java.simpleName
     private lateinit var binding: ActivityBoardInsideBinding
 
+    private lateinit var key: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
-//첫번째 방법
-//        val title = intent.getStringExtra("title").toString()
-//        val content = intent.getStringExtra("content").toString()
-//        val time = intent.getStringExtra("time").toString()
-//
-//        binding.titleArea.text = title
-//        binding.contentArea.text = content
-//        binding.timeArea.text = time
+
+        binding.boardSettingIcon.setOnClickListener {
+            showDialog()
+        }
 
         //두번째 방법
-        val key = intent.getStringExtra("key").toString()
+        key = intent.getStringExtra("key").toString()
         getBoardData(key)
         getImageData(key)
+    }
 
+    private fun showDialog() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+        val mBuilder = AlertDialog.Builder(this).setView(mDialogView).setTitle("게시글 수정/삭제")
+        val alertDialog = mBuilder.show()
+
+        alertDialog.findViewById<Button>(R.id.editBtn)?.setOnClickListener {
+            val intent = Intent(this, BoardEditActivity::class.java)
+            intent.putExtra("key", key)
+            startActivity(intent)
+
+        }
+        alertDialog.findViewById<Button>(R.id.removeBtn)?.setOnClickListener {
+            FBRef.boardRef.child(key).removeValue()
+            Toast.makeText(this, "삭제 완료", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun getImageData(key: String) {
@@ -49,10 +68,10 @@ class BoardInsideActivity : AppCompatActivity() {
 
         val imageView = binding.getImageArea
 
-        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task->
-            if(task.isSuccessful){
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+            if (task.isSuccessful) {
                 Glide.with(this).load(task.result).into(imageView)
-            }else{
+            } else {
 
             }
         })
@@ -62,10 +81,13 @@ class BoardInsideActivity : AppCompatActivity() {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val dataModel = dataSnapshot.getValue(BoardModel::class.java)
-                binding.titleArea.text = dataModel!!.title
-                binding.contentArea.text = dataModel.content
-                binding.timeArea.text = dataModel.time
-
+                if (dataModel != null) {
+                    binding.titleArea.text = dataModel.title
+                    binding.contentArea.text = dataModel.content
+                    binding.timeArea.text = dataModel.time
+                } else {
+                    finish()
+                }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
