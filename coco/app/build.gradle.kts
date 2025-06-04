b4f12d23aed0860d51236ee0bd771d1710a09bcf
baekcii <bsy8470@gmail.com>
2025-06-04 18:58:01 +0900

    feat: setting

--- coco/app/build.gradle.kts
+++ coco/app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
 }
