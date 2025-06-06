74523134e4d34d8ba83d8c3a7d9f95a7a53445a4
baekcii <bsy8470@naver.com>
2025-06-06 22:26:27 +0900

    feat(main): 화면 구현 및 recyclerView,adapter 연결

--- coco/app/src/main/java/com/example/coco/view/main/CoinListFragment.kt
+++ coco/app/src/main/java/com/example/coco/view/main/CoinListFragment.kt
@@ -5,9 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coco.R
+import com.example.coco.databinding.FragmentCoinListBinding
+import com.example.coco.db.entity.InterestCoinEntity
+import com.example.coco.view.adapter.CoinListRVAdapter
+import timber.log.Timber
 
 class CoinListFragment : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    private var _binding: FragmentCoinListBinding? = null
+    private val binding get() = _binding!!
+
+    private val selectedList = ArrayList<InterestCoinEntity>()
+    private val unSelectedList = ArrayList<InterestCoinEntity>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +32,59 @@ class CoinListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coin_list, container, false)
+
+        _binding = FragmentCoinListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getAllInterestCoinData()
+        viewModel.selectedCoinList.observe(viewLifecycleOwner, Observer {
+
+            selectedList.clear()
+            unSelectedList.clear()
+            for (item in it) {
+                if (item.selected) {
+                    selectedList.add(item)
+                } else {
+                    unSelectedList.add(item)
+                }
+            }
+            Timber.d(selectedList.toString())
+            Timber.d(unSelectedList.toString())
+            setSelectedListRV()
+
+        })
+    }
+
+    private fun setSelectedListRV(){
+        val selectRVAdapter = CoinListRVAdapter(requireContext(),selectedList)
+        binding.selectedCoinRV.adapter = selectRVAdapter
+        binding.selectedCoinRV.layoutManager = LinearLayoutManager(requireContext())
+
+        selectRVAdapter.itemClick = object :CoinListRVAdapter.ItemClick{
+            override fun onClick(view: View, position: Int) {
+                Timber.d(selectedList[position].toString())
+            }
+        }
+
+        val unSelectRVAdapter = CoinListRVAdapter(requireContext(),unSelectedList)
+        binding.unSelectedCoinRV.adapter = unSelectRVAdapter
+        binding.unSelectedCoinRV.layoutManager = LinearLayoutManager(requireContext())
+        unSelectRVAdapter.itemClick = object :CoinListRVAdapter.ItemClick{
+            override fun onClick(view: View, position: Int) {
+                Timber.d(unSelectedList[position].toString())
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- coco/app/src/main/java/com/example/coco/view/main/MainViewModel.kt
+++ coco/app/src/main/java/com/example/coco/view/main/MainViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.coco.view.main
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.coco.db.entity.InterestCoinEntity
+import com.example.coco.repository.DBRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val dbRepository = DBRepository()
+
+    lateinit var selectedCoinList: LiveData<List<InterestCoinEntity>>
+
+    fun getAllInterestCoinData() = viewModelScope.launch {
+        val coinList = dbRepository.getAllInterestCoinData().asLiveData()
+        selectedCoinList = coinList
+    }
 }
